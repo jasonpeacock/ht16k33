be65e0ae4f8a783f8b0f64a6ae54d7ef41c37012
Nils Eriksson <nils.edvin.eriksson@gmail.com>
2020-09-13 10:39:02 +0200

    Restored display_data_address.rs somewhat

--- src/types/display_data_address.rs
+++ src/types/display_data_address.rs
@@ -1,32 +1,56 @@
-use num_enum::{IntoPrimitive, TryFromPrimitive};
 use core::fmt;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
 
 use DisplayDataAddress::*;
 
 /// Display RAM data address.
-#[allow(missing_docs, non_camel_case_types)]
+#[allow(non_camel_case_types)]
 #[derive(
     Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, 
     IntoPrimitive, TryFromPrimitive
 )]
 #[repr(u8)]
 pub enum DisplayDataAddress {
-    ROW_0,
-    ROW_1,
-    ROW_2,
-    ROW_3,
-    ROW_4,
-    ROW_5,
-    ROW_6,
-    ROW_7,
-    ROW_8,
-    ROW_9,
-    ROW_10,
-    ROW_11,
-    ROW_12,
-    ROW_13,
-    ROW_14,
-    ROW_15,
+    /// Row 0
+    ROW_0 = 0,
+    /// Row 1
+    ROW_1 = 1,
+    /// Row 2
+    ROW_2 = 2,
+    /// Row 3
+    ROW_3 = 3,
+    /// Row 4
+    ROW_4 = 4,
+    /// Row 5
+    ROW_5 = 5,
+    /// Row 6
+    ROW_6 = 6,
+    /// Row 7
+    ROW_7 = 7,
+    /// Row 8
+    ROW_8 = 8,
+    /// Row 9
+    ROW_9 = 9,
+    /// Row 10
+    ROW_10 = 10,
+    /// Row 11
+    ROW_11 = 11,
+    /// Row 12
+    ROW_12 = 12,
+    /// Row 13
+    ROW_13 = 13,
+    /// Row 14
+    ROW_14 = 14,
+    /// Row 15
+    ROW_15 = 15,
+}
+
+impl DisplayDataAddress {
+    /// Returns the bitvalue
+    #[deprecated(since = "0.1.2", note = "Use `as u8` instead")]
+    pub fn bits(&self) -> u8 {
+        *self as u8
+    }
 }
 
 impl Default for DisplayDataAddress {
